6579cdc1daed7dd61ca77490b45186eee32c27cb
Hieu Vuong Dinh <85358386+hieuvd99@users.noreply.github.com>
2022-03-30 17:21:55 +0700

    Update AdminAccountController.java

--- src/main/java/com/example/shop/adminController/AdminAccountController.java
+++ src/main/java/com/example/shop/adminController/AdminAccountController.java
@@ -80,9 +80,11 @@ public class AdminAccountController {
 	//register
     @RequestMapping("/account/signup")
     public String signup(@Validated @ModelAttribute("item") Account item , BindingResult errors,Model model) throws NoSuchAlgorithmException{
-        if(accountService.existsByUsername(item.getUsername()) || accountService.existsByEmail(item.getEmail())){
+//         if(accountService.existsByUsername(item.getUsername()) || accountService.existsByEmail(item.getEmail())){
+	if(errors.hasErrors()){
             model.addAttribute("message", "Some field are not valid . Please fix them");
-            
+
+//             model.addAttribute("message", "Some field are not valid . Please fix them");            
         }else {
 //         	Account account = new Account();
 //         	MessageDigest md = MessageDigest.getInstance("MD5");
