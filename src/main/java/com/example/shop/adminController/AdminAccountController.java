6fa254b47634be436f9a9582de480ac278659ed3
Hieu Vuong Dinh <85358386+hieuvd99@users.noreply.github.com>
2022-03-30 16:51:23 +0700

    Update AdminAccountController.java

--- src/main/java/com/example/shop/adminController/AdminAccountController.java
+++ src/main/java/com/example/shop/adminController/AdminAccountController.java
@@ -76,31 +76,27 @@ public class AdminAccountController {
 	}
 	
 	//register
-//     @RequestMapping("/account/signup")
-//     public String signup(@Validated @ModelAttribute("item") Account item , BindingResult errors,Model model) throws NoSuchAlgorithmException{
-//         if(accountService.existsByUsername(item.getUsername()) && accountService.existsByEmail(item.getEmail())){
-//             model.addAttribute("message", "Some field are not valid . Please fix them");
-// 		return "layout/register";
-//         }else {
-//         	Account account = new Account();
-//         	MessageDigest md = MessageDigest.getInstance("MD5");
-//             md.update(item.getPassword().getBytes());
-//             byte[] digest = md.digest();
-//             String myHash = DatatypeConverter .printHexBinary(digest);
-            
-//         	account.setUsername(item.getUsername());
-//         	account.setEmail(item.getEmail());
-//         	account.setPassword(myHash);
-//             accountService.save(account);
-//             model.addAttribute("message", "Success") ;
-// 		return "layout/register";
-//         }
-//     }
-	
-	@RequestMapping("/account/signup")
-	public String signup(){
+    @RequestMapping("/account/signup")
+    public String signup(@Validated @ModelAttribute("item") Account item , BindingResult errors,Model model) throws NoSuchAlgorithmException{
+        if(accountService.existsByUsername(item.getUsername()) && accountService.existsByEmail(item.getEmail())){
+            model.addAttribute("message", "Some field are not valid . Please fix them");
 		return "layout/register";
-	}
+        }else {
+        	Account account = new Account();
+        	MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(item.getPassword().getBytes());
+            byte[] digest = md.digest();
+            String myHash = DatatypeConverter .printHexBinary(digest);
+            
+        	account.setUsername(item.getUsername());
+        	account.setEmail(item.getEmail());
+        	account.setPassword(myHash);
+            accountService.save(account);
+            model.addAttribute("message", "Success") ;
+		return "layout/register";
+        }
+    }
+	
 	
     //logout
     @RequestMapping("/account/logout")
